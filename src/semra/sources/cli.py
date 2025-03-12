7f6f7886b3eba3edc8df9b1f80c7ff2a8ccc3746
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-03-12 16:54:48 +0100

    Improve logging

--- src/semra/sources/cli.py
+++ src/semra/sources/cli.py
@@ -7,12 +7,17 @@ from tqdm import tqdm
 @click.command()
 def main() -> None:
     """Run all custom SeMRA mapping getters."""
-    from semra.sources import SOURCE_RESOLVER
+    from semra.sources import SOURCE_RESOLVER, _normalize_name
 
-    for func in tqdm(list(SOURCE_RESOLVER)):
-        tqdm.write(f"Getting mappings with: `{func.__name__}`")
-        m = func()
-        click.echo(f"Got {len(m)} custom mappings")
+    for func in tqdm(list(SOURCE_RESOLVER), desc="Getting SeMRA sources", unit="source"):
+        name = _normalize_name(func)
+        tqdm.write(f"[{name}] getting mappings")
+        try:
+            mappings = func()
+        except Exception as e:
+            tqdm.write(f"[{name}] failed:\n{e}")
+        else:
+            tqdm.write(f"[{name}] got {len(mappings):,} mappings")
 
 
 if __name__ == "__main__":

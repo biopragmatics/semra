6842ac85522bf8b945d96bcc8c29e76b144bd89b
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-03-21 10:37:21 +0100

    Update run.sh

--- notebooks/landscape/run.sh
+++ notebooks/landscape/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 for file in */*.ipynb; do
     # you can add --debug to this command to get all of the output
     jupyter nbconvert --execute --to notebook --output $file $file

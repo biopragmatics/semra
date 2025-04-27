529f69315b38a5eea99a722d7effaa5c0e48fc83
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-04-27 13:49:19 +0200

    Test all

--- tests/test_neo4j_output.py
+++ tests/test_neo4j_output.py
@@ -13,9 +13,10 @@ from semra import (
     MappingSet,
     Reference,
     SimpleEvidence,
+    ReasonedEvidence,
 )
 from semra.io import write_neo4j
-from semra.rules import BEN_ORCID, charlie
+from semra.rules import BEN_ORCID, charlie, KNOWLEDGE_MAPPING, CHAIN_MAPPING, UNSPECIFIED_MAPPING
 from tests import resources
 
 
@@ -24,44 +25,89 @@ class TestNeo4jOutput(unittest.TestCase):
 
     def test_neo4j_output(self) -> None:
         """Test Neo4j output."""
+        r1 = Reference.from_curie("mesh:C406527", name="R 115866")
+        r2 = Reference.from_curie("chebi:101854", name="talarozole")
+        r3 = Reference.from_curie("chembl.compound:CHEMBL459505", name="TALAROZOLE")
+
+        t1 = r1, EXACT_MATCH, r2
+        t2 = r2, EXACT_MATCH, r3
+        t3 = r1, EXACT_MATCH, r3
+
         biomappings = MappingSet(
             name="biomappings",
             confidence=0.90,
             license="CC0",
         )
-        m1 = Mapping(
-            s=Reference.from_curie("chebi:101854", name="talarozole"),
-            p=EXACT_MATCH,
-            o=Reference.from_curie("mesh:C406527", name="R 115866"),
-            evidence=[
-                SimpleEvidence(
-                    mapping_set=biomappings,
-                    justification=MANUAL_MAPPING,
-                    author=charlie,
-                    confidence=0.99,
-                ),
-                SimpleEvidence(
-                    mapping_set=biomappings,
-                    justification=MANUAL_MAPPING,
-                    author=BEN_ORCID,
-                    confidence=0.94,
-                ),
-                SimpleEvidence(
-                    mapping_set=MappingSet(
-                        name="lexical",
-                        confidence=0.90,
-                    ),
-                    justification=LEXICAL_MAPPING,
-                    confidence=0.8,
-                ),
-            ],
+        self.assertEqual("fa68b8e87141ffac7e64d2be07ef4b62", biomappings.hexdigest())
+
+        chembl = MappingSet(
+            name="chembl",
+            confidence=0.90,
+            license="CC-BY-SA-3.0",
         )
-        mappings: list[semra.Mapping] = [m1]
+
+        m1_e1 = SimpleEvidence(
+            mapping_set=biomappings,
+            justification=MANUAL_MAPPING,
+            author=charlie,
+            confidence=0.99,
+        )
+        self.assertEqual("b6218d883b277224a0bc92746e829f64", m1_e1.hexdigest(t1))
+
+        m1_e2 = SimpleEvidence(
+            mapping_set=biomappings,
+            justification=MANUAL_MAPPING,
+            author=BEN_ORCID,
+            confidence=0.94,
+        )
+        m1_e3 = SimpleEvidence(
+            mapping_set=MappingSet(
+                name="lexical",
+                confidence=0.90,
+            ),
+            justification=LEXICAL_MAPPING,
+            confidence=0.8,
+        )
+
+        m1 = Mapping.from_triple(
+            t1,
+            evidence=[m1_e1, m1_e2, m1_e3],
+        )
+
+        # this curie is generated as a md5 digest of the pickle dump
+        # of the 3-tuple of CURIE strings for the subject, predicate, object
+        m1_hexdigest = "f9705b214dbb3feb51ea49e973e0b8c3"
+        self.assertEqual(m1_hexdigest, m1.hexdigest())
+
+        # Test that the evidences don't affect the hash
+        for x in [
+            Mapping.from_triple(t1),
+            Mapping.from_triple(t1, evidence=[m1_e1]),
+            Mapping.from_triple(t1, evidence=[m1_e2]),
+            Mapping.from_triple(t1, evidence=[m1_e3]),
+            Mapping.from_triple(t1, evidence=[m1_e2, m1_e1]),
+            Mapping.from_triple(t1, evidence=[m1_e2, m1_e1, m1_e3]),
+        ]:
+            self.assertEqual(m1_hexdigest, x.hexdigest())
+
+        m2_e1 = SimpleEvidence(
+            mapping_set=chembl,
+            justification=UNSPECIFIED_MAPPING,
+            confidence=0.90,
+        )
+        m2 = Mapping.from_triple(t2, evidence=[m2_e1])
+
+        m3_e1 = ReasonedEvidence(
+            justification=CHAIN_MAPPING,
+            mappings=[m1, m2]
+        )
+        m3 = Mapping.from_triple(t3, evidence=[m3_e1])
+
+        mappings: list[semra.Mapping] = [m1, m2, m3]
 
         with tempfile.TemporaryDirectory() as _directory:
             directory = Path(_directory)
 
-            # write_neo4j(mappings, "/Users/cthoyt/Desktop/test-neo4j", use_tqdm=False)
             write_neo4j(mappings, directory, use_tqdm=False)
 
             # this test is important since it makes sure we get deterministic hashes each time

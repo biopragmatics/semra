f547a19fcc4eec337a152ca648904fbddce66e93
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-04-29 11:40:46 +0200

    Update test_io.py

--- tests/test_io.py
+++ tests/test_io.py
@@ -13,14 +13,15 @@ from semra.io import (
     from_digraph,
     from_jsonl,
     from_multidigraph,
+    from_pickle,
     from_pyobo,
-    from_sssom_df,
     from_sssom,
-    write_sssom,
+    from_sssom_df,
     to_digraph,
     to_multidigraph,
-    from_pickle, write_pickle,
     write_jsonl,
+    write_pickle,
+    write_sssom,
 )
 from semra.rules import (
     BEN_ORCID,

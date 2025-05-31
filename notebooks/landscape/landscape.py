e815d71a485084761e8a61f1ffaab129056ba69c
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-05-31 11:53:50 +0200

    Update landscape.py

--- notebooks/landscape/landscape.py
+++ notebooks/landscape/landscape.py
@@ -8,6 +8,7 @@ from semra.landscape.cli import compile_landscape_metaanalysis
 @click.command()
 def main() -> None:
     """Generate a summary over the landscape analyses."""
+    click.echo("Note that this script is now part of the main `semra landscape` workflow")
     compile_landscape_metaanalysis()
 
 

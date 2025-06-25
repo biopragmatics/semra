8e1827dc55981c97044b729819bb09a424d0a0c8
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-06-25 14:14:54 +0200

    Update biomappings

--- src/semra/sources/biopragmatics.py
+++ src/semra/sources/biopragmatics.py
@@ -2,15 +2,8 @@
 
 from __future__ import annotations
 
-import importlib.metadata
-from collections.abc import Iterable
-from typing import Any
-
-import pandas as pd
-from pyobo import Reference
-from tqdm.asyncio import tqdm
-
-from semra.struct import Mapping, MappingSet, SimpleEvidence
+from semra.io import from_sssom
+from semra.struct import Mapping
 
 __all__ = [
     "from_biomappings_negative",
@@ -22,92 +15,45 @@ __all__ = [
 def get_biomappings_positive_mappings() -> list[Mapping]:
     """Get positive mappings from Biomappings."""
     try:
-        import biomappings
+        import biomappings.resources
     except ImportError:
-        return read_remote_tsv("mappings.tsv")
+        return read_remote_tsv("positive.sssom.tsv")
     else:
-        return _process(biomappings.load_mappings())
+        return from_sssom(
+            biomappings.resources.POSITIVES_SSSOM_PATH,
+            mapping_set_title="Biomappings",
+        )
 
 
 def from_biomappings_negative() -> list[Mapping]:
     """Get negative mappings from Biomappings."""
     try:
-        import biomappings
+        import biomappings.resources
     except ImportError:
-        return read_remote_tsv("incorrect.tsv")
+        return read_remote_tsv("negative.sssom.tsv")
     else:
-        return _process(biomappings.load_false_mappings())
+        return from_sssom(
+            biomappings.resources.NEGATIVES_SSSOM_PATH,
+            mapping_set_title="Biomappings",
+        )
 
 
 def from_biomappings_predicted() -> list[Mapping]:
     """Get predicted mappings from Biomappings."""
     try:
-        import biomappings
+        import biomappings.resources
     except ImportError:
-        return read_remote_tsv("predictions.tsv")
+        return read_remote_tsv("predictions.sssom.tsv")
     else:
-        return _process(biomappings.load_predictions())
+        return from_sssom(
+            biomappings.resources.PREDICTIONS_SSSOM_PATH,
+            mapping_set_title="Biomappings",
+        )
 
 
 BASE_URL = "https://github.com/biopragmatics/biomappings/raw/master/src/biomappings/resources"
 
 
 def read_remote_tsv(name: str) -> list[Mapping]:
-    """Load a remote mapping file from the Biomappings github repository."""
-    url = f"{BASE_URL}/{name}"
-    df = pd.read_csv(url, sep="\t")
-    mapping_dicts = df.to_json(orient="records")
-    return _process(mapping_dicts)
-
-
-def _process(mapping_dicts: Iterable[dict[str, Any]], confidence: float = 0.999) -> list[Mapping]:
-    try:
-        biomappings_version = importlib.metadata.version("biomappings")
-    except Exception:
-        biomappings_version = None
-    mapping_set = MappingSet(
-        name="biomappings", confidence=confidence, license="CC0", version=biomappings_version
-    )
-    rv = []
-    for mapping_dict in tqdm(
-        mapping_dicts, unit_scale=True, unit="mapping", desc="Loading biomappings", leave=False
-    ):
-        if (source := mapping_dict["source"]).startswith("orcid"):
-            author = Reference.from_curie(source)
-        else:
-            author = None
-        subject = Reference(
-            prefix=mapping_dict["source prefix"],
-            identifier=mapping_dict["source identifier"],
-        )
-        obj = Reference(
-            prefix=mapping_dict["target prefix"],
-            identifier=mapping_dict["target identifier"],
-        )
-        predicate = Reference.from_curie(mapping_dict["relation"])
-        if predicate is None:
-            continue
-        # TODO remove below?
-        if predicate.curie == "oboinowl:hasDbXref":
-            predicate = Reference(
-                prefix="oboInOwl", identifier="hasDbXref", name="has database cross-reference"
-            )
-        elif predicate.curie == "skos:exactMatch":
-            predicate = Reference(prefix="skos", identifier="exactMatch", name="exact match")
-
-        mm = Mapping(
-            subject=subject,
-            predicate=predicate,
-            object=obj,
-            evidence=[
-                SimpleEvidence(
-                    justification=Reference.from_curie(mapping_dict["type"]),
-                    mapping_set=mapping_set,
-                    author=author,
-                    # TODO configurable confidence globally per author or
-                    #  based on author's self-reported confidence
-                )
-            ],
-        )
-        rv.append(mm)
-    return rv
+    """Load a remote mapping file from the Biomappings GitHub repository."""
+    return from_sssom(f"{BASE_URL}/{name}")

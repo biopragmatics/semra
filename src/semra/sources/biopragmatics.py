054671f6638075198449a098296610969193cad8
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-06-29 08:45:19 +0200

    Update biopragmatics.py

--- src/semra/sources/biopragmatics.py
+++ src/semra/sources/biopragmatics.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+from typing import Any
+
 from semra.io import from_sssom
 from semra.struct import Mapping
 
@@ -11,49 +13,54 @@ __all__ = [
     "get_biomappings_positive_mappings",
 ]
 
+_NAME = "Biomappings"
 
-def get_biomappings_positive_mappings() -> list[Mapping]:
+
+def get_biomappings_positive_mappings(*, remote_only: bool = False) -> list[Mapping]:
     """Get positive mappings from Biomappings."""
+    if remote_only:
+        return read_remote_tsv("positive.sssom.tsv", mapping_set_title=_NAME)
+
     try:
-        import biomappings.resources
+        from biomappings.utils import POSITIVES_SSSOM_PATH
     except ImportError:
-        return read_remote_tsv("positive.sssom.tsv")
-    else:
-        return from_sssom(
-            biomappings.resources.POSITIVES_SSSOM_PATH,
-            mapping_set_title="Biomappings",
+        return read_remote_tsv(
+            "positive.sssom.tsv",
+            mapping_set_title=_NAME,
         )
+    else:
+        return from_sssom(POSITIVES_SSSOM_PATH, mapping_set_title=_NAME)
 
 
-def from_biomappings_negative() -> list[Mapping]:
+def from_biomappings_negative(*, remote_only: bool = False) -> list[Mapping]:
     """Get negative mappings from Biomappings."""
+    if remote_only:
+        return read_remote_tsv("negative.sssom.tsv", mapping_set_title=_NAME)
+
     try:
-        import biomappings.resources
+        from biomappings.utils import NEGATIVES_SSSOM_PATH
     except ImportError:
-        return read_remote_tsv("negative.sssom.tsv")
+        return read_remote_tsv("negative.sssom.tsv", mapping_set_title=_NAME)
     else:
-        return from_sssom(
-            biomappings.resources.NEGATIVES_SSSOM_PATH,
-            mapping_set_title="Biomappings",
-        )
+        return from_sssom(NEGATIVES_SSSOM_PATH, mapping_set_title=_NAME)
 
 
-def from_biomappings_predicted() -> list[Mapping]:
+def from_biomappings_predicted(*, remote_only: bool = False) -> list[Mapping]:
     """Get predicted mappings from Biomappings."""
+    if remote_only:
+        return read_remote_tsv("predictions.sssom.tsv", mapping_set_title=_NAME)
+
     try:
-        import biomappings.resources
+        from biomappings.utils import PREDICTIONS_SSSOM_PATH
     except ImportError:
-        return read_remote_tsv("predictions.sssom.tsv")
+        return read_remote_tsv("predictions.sssom.tsv", mapping_set_title="Biomappings")
     else:
-        return from_sssom(
-            biomappings.resources.PREDICTIONS_SSSOM_PATH,
-            mapping_set_title="Biomappings",
-        )
+        return from_sssom(PREDICTIONS_SSSOM_PATH, mapping_set_title="Biomappings")
 
 
 BASE_URL = "https://github.com/biopragmatics/biomappings/raw/master/src/biomappings/resources"
 
 
-def read_remote_tsv(name: str) -> list[Mapping]:
+def read_remote_tsv(name: str, **kwargs: Any) -> list[Mapping]:
     """Load a remote mapping file from the Biomappings GitHub repository."""
-    return from_sssom(f"{BASE_URL}/{name}")
+    return from_sssom(f"{BASE_URL}/{name}", **kwargs)

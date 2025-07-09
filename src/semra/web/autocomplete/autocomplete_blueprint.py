d008db264b277e7171182b754f14b29827510f17
kkaris <karis.klas@gmail.com>
2025-07-09 12:48:43 -0700

    Reimplement search with full-text index

--- src/semra/web/autocomplete/autocomplete_blueprint.py
+++ src/semra/web/autocomplete/autocomplete_blueprint.py
@@ -1,16 +1,32 @@
 import fastapi
 
-from semra.web.autocomplete import ConceptsTrie, Entry
+from typing import Annotated
+from semra.client import BaseClient
 
 auto_router = fastapi.APIRouter(prefix="/autocomplete")
 
 
-# Initialize the autocomplete trie
-trie = ConceptsTrie.from_graph_db()
+def _fastapi_get_client(request: fastapi.Request) -> BaseClient:
+    return request.app.state.client  # type:ignore
 
 
-@auto_router.get("/search", response_model=list[Entry])
-def autocomplete_search(prefix: str, top_n: int = 100):
+AnnotatedClient = Annotated[BaseClient, fastapi.Depends(_fastapi_get_client)]
+
+@auto_router.get("/search", response_model=list[list[str]])
+def autocomplete_search(
+    client: AnnotatedClient,
+    prefix: str,
+    top_n: int = 100,
+):
     """Get the autocomplete suggestions for a given prefix."""
+    if ":" in prefix:
+        # Just return an empty response if the prefix contains a colon
+        return []
+    prefix_ = prefix + "~" if not prefix.endswith("~") else prefix
     top_n = min(top_n, 100)
-    return trie.case_insensitive_search(prefix, top_n=top_n)
+    query = """\
+    CALL db.index.fulltext.queryNodes("concept_name_ft", $prefix) YIELD node
+    RETURN toLower(node.name), node.name, node.curie
+    LIMIT $top_n"""
+    res = client.read_query(query, top_n=top_n, prefix=prefix_)
+    return res

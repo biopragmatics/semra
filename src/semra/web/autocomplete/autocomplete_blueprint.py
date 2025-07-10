008605cd0c713200fecb207fee4377b6595b8d95
kkaris <karis.klas@gmail.com>
2025-07-09 21:28:01 -0700

    Only return name and curie

--- src/semra/web/autocomplete/autocomplete_blueprint.py
+++ src/semra/web/autocomplete/autocomplete_blueprint.py
@@ -12,7 +12,7 @@ def _fastapi_get_client(request: fastapi.Request) -> BaseClient:
 
 AnnotatedClient = Annotated[BaseClient, fastapi.Depends(_fastapi_get_client)]
 
-@auto_router.get("/search", response_model=list[list[str]])
+@auto_router.get("/search", response_model=list[list[str | None]])
 def autocomplete_search(
     client: AnnotatedClient,
     prefix: str,
@@ -27,7 +27,7 @@ def autocomplete_search(
 
     query = f"""\
     CALL db.index.fulltext.queryNodes("concept_curie_name_ft", $prefix) YIELD node
-    RETURN toLower(node.name), node.name, node.curie
+    RETURN node.name, node.curie
     LIMIT $top_n
     """
     res = client.read_query(query, top_n=top_n, prefix=prefix_clause)

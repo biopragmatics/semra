6c9144156be003d94890574de7dc9ee5d4e43c8a
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-06-12 12:06:11 +0200

    Update disease.py

--- src/semra/landscape/disease.py
+++ src/semra/landscape/disease.py
@@ -50,12 +50,12 @@ The SeMRA Disease Mappings Database can be rebuilt with the following commands:
 
 .. note::
 
-    Downloading raw data resources can take on the order of hours to tens
-    of hours depending on your internet connection and the reliability of
-    the resources' respective servers.
+    Downloading raw data resources can take on the order of hours to tens of hours
+    depending on your internet connection and the reliability of the resources'
+    respective servers.
 
-    Processing and analysis can be run overnight on commodity hardware
-    (e.g., a 2023 MacBook Pro with 36GB RAM).
+    Processing and analysis can be run overnight on commodity hardware (e.g., a 2023
+    MacBook Pro with 36GB RAM).
 
 Web Application
 ***************

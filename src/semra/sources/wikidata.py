58260d1570df015a7b826207707fbac28ef9191c
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-09-19 15:12:55 +0200

    Update

--- src/semra/sources/wikidata.py
+++ src/semra/sources/wikidata.py
@@ -2,6 +2,7 @@
 
 import datetime
 from collections.abc import Iterable
+from typing import Unpack
 
 import bioregistry
 import pystow
@@ -10,8 +11,13 @@ from curies import vocabulary as v
 from pydantic import AnyUrl
 from sssom_pydantic import MappingSet, SemanticMapping
 from sssom_pydantic.constants import CC0_URL
-from sssom_pydantic.contrib.wikidata import get_mappings_by_prefix, get_wikidata_property_mappings
+from sssom_pydantic.contrib.wikidata import (
+    get_equivalent_property_mappings,
+    get_exact_match_mappings,
+    get_mappings_by_property,
+)
 from tqdm import tqdm
+from wikidata_client import QueryKwargs
 
 __all__ = [
     "get_wikidata_mappings",
@@ -20,18 +26,19 @@ __all__ = [
 
 
 def get_wikidata_mappings(
-    *, progress: bool = True, endpoint: str | None = None
+    *, progress: bool = True, **kwargs: Unpack[QueryKwargs]
 ) -> list[SemanticMapping]:
     """Iterate over WikiData xref dataframes."""
+    converter = bioregistry.get_default_converter()
     return [
-        *get_wikidata_property_mappings(endpoint=endpoint),
-        # TODO get exact matches
-        *_get_all_wikidata_mappings(endpoint=endpoint, progress=progress),
+        *get_equivalent_property_mappings(converter=converter, **kwargs),
+        *get_exact_match_mappings(converter=converter, **kwargs),
+        *_get_all_wikidata_mappings(progress=progress, **kwargs),
     ]
 
 
 def _get_all_wikidata_mappings(
-    *, progress: bool = True, endpoint: str | None = None
+    *, progress: bool = True, **kwargs: Unpack[QueryKwargs]
 ) -> Iterable[SemanticMapping]:
     """Iterate over WikiData xref dataframes."""
     wikidata_properties = bioregistry.get_registry_map("wikidata")
@@ -41,35 +48,36 @@ def _get_all_wikidata_mappings(
             continue  # too many
         it.set_postfix({"prefix": prefix, "prop": wikidata_property})
         try:
-            yield from get_wikidata_mappings_by_prefix(prefix=prefix, endpoint=endpoint)
+            yield from get_wikidata_mappings_by_prefix(prefix=prefix, **kwargs)
         except OSError:
             tqdm.write(f"failed to get {prefix}/{wikidata_property}")
             continue
 
 
 def get_wikidata_mappings_by_prefix(
-    prefix: str, *, endpoint: str | None = None
+    prefix: str, **kwargs: Unpack[QueryKwargs]
 ) -> Iterable[SemanticMapping]:
     """Get mappings from Wikidata."""
-    prefix_to_prop = bioregistry.get_registry_map("wikidata")
-    prop = prefix_to_prop[prefix]
-    path = pystow.join("wikidata", "mappings", name=f"{prefix}-{prop}.sssom.tsv.gz")
+    property_id = bioregistry.get_registry_map("wikidata")[prefix]
+    path = pystow.join("wikidata", "mappings", name=f"{prefix}-{property_id}.sssom.tsv.gz")
     if path.is_file():
-        tqdm.write(f"reading cache for {prefix}/{prop} at {path}")
+        tqdm.write(f"reading cache for {prefix}/{property_id} at {path}")
         with sssom_pydantic.read_iterable(path) as file:
             for record in file.mappings:
                 if isinstance(record, SemanticMapping):
                     yield record
     else:
         metadata = MappingSet(
-            id=AnyUrl(f"https://w3id.org/biopragmatics/mappings/wikidata/{prop}.sssom.tsv.gz"),
+            id=AnyUrl(
+                f"https://w3id.org/biopragmatics/mappings/wikidata/{property_id}.sssom.tsv.gz"
+            ),
             license=AnyUrl(CC0_URL),
             creators=[v.charlie],
             confidence=0.99,
             publication_date=datetime.date.today(),
         )
         converter = bioregistry.get_default_converter()
-        mappings = list(get_mappings_by_prefix(prefix=prefix, property_id=prop, endpoint=endpoint))
+        mappings = list(get_mappings_by_property(prefix=prefix, property_id=property_id, **kwargs))
         sssom_pydantic.write(mappings=mappings, path=path, metadata=metadata, converter=converter)
         yield from mappings
 
